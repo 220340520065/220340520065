e3f2f7e07141471c33ea4dd060901a03bee8e8c8
Parth Patil <101080790+220340520065@users.noreply.github.com>
2022-05-05 13:33:51 +0530

    Add files via upload

---
+++ order.java
@@ -0,0 +1,10 @@
+
+package mypack;
+import java.util.*;
+public class order 
+{
+	public String food_name;
+      public double total_value;
+	public int size;
+	public static double total_final_bill=0;
+}
